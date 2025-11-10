71d77ea5fd9c4266b4b45c7a04b6c5e42aa8224b
dodi <mischl.akos@ding.hu>
2025-11-10 19:41:51 +0100

    initial commit

---
+++ glpi-docker/storage/glpi/config/config_db.php
@@ -0,0 +1,11 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost = 'db:3306';
+   public $dbuser = 'glpi';
+   public $dbpassword = 'ErosJelszo123%21';
+   public $dbdefault = 'glpi';
+   public $use_timezones = true;
+   public $use_utf8mb4 = true;
+   public $allow_datetime = false;
+   public $allow_signed_keys = false;
+}
